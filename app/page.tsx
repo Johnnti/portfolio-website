df10c0bd9d954fcf67063c345aaaa738d6b9e67e
John <dejhswavyguy@gmail.com>
2025-03-05 12:36:56 -0600

    add resume download functionality

--- app/page.tsx
+++ app/page.tsx
@@ -9,17 +9,19 @@ export default function Home() {
       <header className="p-5">
         <nav className="flex justify-end text-lg space-x-6">
           <motion.a whileHover={{ scale: 1.1 }} href="#" className="hover:text-orange-400">Profile</motion.a>
-          <motion.a whileHover={{ scale: 1.1 }} href="#" className="hover:text-orange-400">Resume</motion.a>
+          <motion.a 
+
+          whileHover={{ scale: 1.1 }} href="/John_Resume.pdf" download = "John_Nti_Anokye_Resume.pdf"className="hover:text-orange-400">Resume</motion.a>
           <motion.a whileHover={{ scale: 1.1 }} href="#" className="hover:text-orange-400">Projects</motion.a>
         </nav>
       </header>
 
       {/* Main Content */}
       <main className="flex flex-col justify-center items-center flex-grow text-center">
-        <motion.h1 initial={{ opacity: 0, y: -20 }} animate={{ opacity: 1, y: 0 }} transition={{ duration: 1 }} className="text-5xl font-bold">
+        <motion.h1 initial={{ opacity: 0, y: -20 }} animate={{ opacity: 1, y: 0 }} transition={{ duration: 0.5 }} className="text-5xl font-bold">
           John Nti Anokye
         </motion.h1>
-        <motion.h2 initial={{ opacity: 0, y: 20 }} animate={{ opacity: 1, y: 0 }} transition={{ duration: 1, delay: 0.3 }} className="text-2xl mt-4 bg-orange-500 text-white px-4 py-2 rounded-lg">
+        <motion.h2 initial={{ opacity: 0, y: 20 }} animate={{ opacity: 1, y: 0 }} transition={{ duration: 0.5, delay: 0.3 }} className="text-2xl mt-4 bg-orange-500 text-white px-4 py-2 rounded-lg">
           Computer Science Student
         </motion.h2>
 
